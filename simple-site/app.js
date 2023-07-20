60b557f1f2755b33afa865bc2d49194636e22a73
Giovanna Capitani <133259476+Gi-Capitani@users.noreply.github.com>
2023-07-20 16:04:01 -0300

    Update app.js

--- simple-site/app.js
+++ simple-site/app.js
@@ -1,2 +1,23 @@
-'use strict';
-const switcher = document.querySelector('.btn');
+// Obtém todos os botões com a classe "botoes-carrossel"
+const botoes = document.querySelectorAll('.botoes-carrossel');
+const imagens = document.querySelectorAll('.carrossel');
+
+// Adiciona um ouvinte de eventos para cada botão
+botoes.forEach(botao => {
+  botao.addEventListener('click', exibirImagem);
+});
+
+function exibirImagem(event) {
+  const target = event.target;
+  const imagemId = target.getAttribute('data-target');
+  const imagem = document.getElementById(imagemId);
+
+  // Esconde todas as imagens
+  
+  imagens.forEach(img => {
+    imagem.style.display = 'none';
+  });
+
+  // Exibe a imagem correspondente ao botão clicado
+  imagem.style.display = 'block';
+};
